3d8697bc2c05a6c87f001182a0b5c0a2c6cae94d
tubingtime <tdelaveaga@dons.usfca.edu>
2023-02-16 08:17:48 -0800

    update using sarah's schma

--- src/app.ts
+++ src/app.ts
@@ -49,26 +49,23 @@ async function setupSchema(){
 }
 
 
-// TODO: rewrite using sarah's schema
-// TODO: Create schema if not exists
-
-
-
 /**
- * Display a message on the home page.
+ * Displays a message on the home page.
  */
-app.get("/", async (req, res) => {
+ app.get("/", async (req, res) => {
     try {
-        res.end("<h1> This is the TODO API</h1>");
+        res.end("Welcome to TODO list");
     } catch (err: any) {
         console.error(err.message);
     }
 })
 
-// Create new todo list 
+/**
+ * Creates a TodoList.
+ */
 app.post("/lists", async (req, res) => {
     try {
-        const name = req.body.name;
+        const { name } = req.body;
         const newList = await pool.query("INSERT INTO Lists (name) VALUES($1) RETURNING *", [name]);
         res.json(newList.rows[0]);
     } catch (err: any) {
@@ -76,61 +73,112 @@ app.post("/lists", async (req, res) => {
     }
 })
 
-// Insert new todo item 
-app.post("/todos", async (req, res) => {
+/**
+ * Creates and adds a TodoItem to a specific list.
+ */
+app.post("/lists/id/:list_id", async (req, res) => {
     try {
-        const description = req.body.description;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
-        console.log(newTodo);
-        res.json(newTodo.rows[0]);
-    } catch (err: any) {
-        console.error(err.message)
-    }
-});
-
-app.get("/todos", async (req, res) => {
-    try {
-        const allTodos = await pool.query("SELECT * FROM todo");
-        res.json(allTodos.rows);
+        const { list_id } = req.params;
+        const { description } = req.body;
+        const newTodoItem = await pool.query("INSERT INTO Items (list_id, description) VALUES($1, $2)", [list_id, description]);
+        res.json("Added new TodoItem!");
     } catch (err: any) {
         console.error(err.message);
     }
-});
+})
 
-app.get("/todos/:id", async (req, res) => {
+/**
+ * Gets all of the TodoLists.
+ */
+app.get("/lists", async (req, res) => {
+    try {
+        const lists = await pool.query("SELECT * from lists");
+        res.json(lists.rows);
+    } catch (err: any) {
+        console.error(err.message);
+    }
+})
+
+/**
+ * Get all TodoTtems in a TodoList by list_id.
+ */
+app.get("/lists/id/:list_id", async (req, res) => {
+    try {
+        const { list_id } = req.params;
+        const items = await pool.query("SELECT * FROM items WHERE list_id = $1", [list_id]);
+        res.json(items.rows);
+    } catch (err: any) {
+        console.error(err.message);
+    }
+})
+
+/**
+ * Get all TodoItems in a TodoList by list_name.
+ */
+app.get("/lists/name/:list_name", async (req, res) => {
+    try {
+        const { list_name } = req.params;
+        const items = await pool.query("SELECT i.id, i.list_id, i.description, i.completed, i.list_id FROM items i JOIN lists l ON i.list_id = l.id WHERE l.name = $1", [list_name]);
+        res.json(items.rows);
+    } catch (err: any) {
+        console.error(err.message);
+    }
+})
+
+/**
+ * Updates a TodoItem and marks it as done.
+ */
+app.put("/items/id/:id", async (req, res) => {
     try {
         const { id } = req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
-        res.json(todo.rows[0]);
+        const updateItem = await pool.query("UPDATE Items SET completed = true WHERE id = $1", [id]);
+        res.json("TodoItem was marked done!");
     } catch (err: any) {
         console.error(err.message);
     }
-});
+})
 
-app.put("/todos/:id", async (req, res) => {
-    try {
-        const id  = req.params.id;
-        const description = req.body.description;
-        const updateTodo = await pool.query(
-            "UPDATE todo SET description = $1 WHERE todo_id = $2",
-            [description, id]
-        );
-
-        res.json("Todo was updated!");
-    } catch (err: any) {
-        console.error(err.message);
-    }
-});
-
-app.delete("/todos/:id", async (req, res) => {
+/**
+ * Get a TodoItem by item id.
+ */
+app.get("/items/id/:id", async (req, res) => {
     try {
         const { id } = req.params;
-        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
-        res.json("Todo was deleted!");
+        const item = await pool.query("SELECT * FROM items WHERE id = $1", [id]);
+        res.json(item.rows[0]);
     } catch (err: any) {
         console.error(err.message);
     }
-});
+})
+
+/**
+ * Deletes a TodoItem.
+ */
+app.delete("/items/id/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodoItem = await pool.query("DELETE FROM items WHERE id = $1", [
+            id: any
+        ]);
+        res.json("TodoItem was deleted!");
+    } catch (err: any) {
+        console.log(err.message);
+    }
+})
+
+/**
+ * Deletes a TodoList.
+ */
+app.delete("/lists/id/:list_id", async (req, res) => {
+    try {
+        const { list_id } = req.params;
+        const deleteTodoItems = await pool.query("DELETE FROM Items where list_id = $1", [list_id]);
+        const deleteTodoList = await pool.query("DELETE FROM lists WHERE id = $1", [list_id]);
+        res.json("TodoItem was deleted!");
+    } catch (err: any) {
+        console.log(err.message);
+    }
+})
 
 app.listen(process.env.API_PORT, () => {
     console.log(`Server is running on port${process.env.API_PORT}`);
